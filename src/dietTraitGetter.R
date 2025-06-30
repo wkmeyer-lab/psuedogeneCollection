e8892e9510c4cc8d29d072015a9465d15eb8a2b0
Tyler Gruver <tyler.l.gruver@gmail.com>
2025-06-29 21:21:15 -0400

    cleanSpecies works

--- src/dietTraitGetter.R
+++ src/dietTraitGetter.R
@@ -4,69 +4,55 @@
 #'
 #'@issue: species names need to be the same for comparison
 #'        not the same at the moment
-#' /
+#'
 
+library(here)
 
-getwd()
 main <- function(){
-  dirtyData <- read.csv("../data/mergedData")
+
+  dirtyData <- read.csv(here("data", "mergedData.csv"))
   
   species <- dirtyData[,3] # obtains scientific name
   cleanLiSpecies <- cleanSpecies(species)
+  View(cleanLiSpecies)
   
   diet <- dirtyData[,length(dirtyData)] #obtains diet type
   
   Spec_Diet <- data.frame( species = species, diet = diet)
   
-  write.table(Spec_Diet, "../output/dietTraits", sep = "\t", row.names = FALSE, quote = FALSE)
-  return( list(species, diet, Spec_Diet) )
+  write.table(Spec_Diet, here("data, dietTraits"), sep = "\t", row.names = FALSE, quote = FALSE)
+  return( list(species, diet, Spec_Diet, cleanLiSpecies) )
 }
 
-
- temp <- main()
-
- #' Method to include species only present in "species" within listOfSpecies
+ #'@method to find the same string between two lists
  #'@param species all species obtained from merged data
+ #'@return list containing all species that are the same in both species within listOfSpecies and mergedData
  cleanSpecies <- function(species){
-  allSpecies <- as.list(read.table("../data/listOfSpecies")) # 1 species/row --> n elements in list
-   
-   for(i in 1:length(allSpecies)){
+  allSpDirty <- read.table(here("data", "ListOfSpecies"))[,1] # 1 species/row --> n elements in list
+  allSpClean <- truncateSp(allSpDirty)
+  
+  sameSpecies = c()
+   for(i in 1:length(allSpClean)){
      for( n in 1:length(species)){
-       if(species[n] == 1:length(allSpecies[i]) ){
-         allSpecies =  allSpecies[-i]
+       if(species[n] == allSpClean[i]){
+         sameSpecies = c(sameSpecies, allSpClean[i])
        }
      }
    }
    
-   return(allSpecies)
+   return(sameSpecies)
    
  }
- 
- testlistOfSpeciesIsClean <- function(){
-   
+#' @Method to keep only scientific name, remove '_' and replace w space
+#' @param sp is species name obtained from listofspecies
+ truncateSp <- function(sp){
+  #truncate by looking for first instance of '__'
+   truncated <- sub("__.*$", "", sp)  #. including, * until $ end of line
+   smallSp <- gsub("_", " ", truncated) #see also grep
+   return (smallSp)
  }
  
- #' Test to obtain species and compare against listOfSpecies file to ensure all species are present
- testComparingSpeciesFiles <- function(testVars){
-    sMerged <- testVars[[1]]
-    sLossSum <- readLines("../output/ListOfSpecies")
-    
-    if(length(sMerged) < length(sLossSum)){
-      print(" More Loss Sum Species than from MergedData.csv")
-      cat( "losssum species count: ", length(sLossSum), " || mergedData species count: ", length(sMerged))
-    }
-    
- }
+ temp <- main()
  
- #' Each value was compared visually to ensure I grabbed the intended cols
- #' @param  testVars contains test variables
- testVisual <- function(testVars) {
-   View(testVars[[1]])
-   View(testVars[[2]])
-   View(testVars[[3]])
- }
- 
- #testVisual(temp)
- #testComparingSpeciesFiles(temp)
  
  
